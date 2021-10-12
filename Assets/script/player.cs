d7d92904fd5ae27df670064543ba06ae0f22cbe7
SammySealAngel <72489260+SammySealAngel@users.noreply.github.com>
2021-10-12 13:50:02 +0700

    initial

---
+++ Assets/script/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            animator.SetBool("isbreakdance", true);
+
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            animator.SetBool("isbreakdance", false);
+        }
+    }
+}
